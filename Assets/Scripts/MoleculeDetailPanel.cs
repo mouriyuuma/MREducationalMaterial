39bb874af6023c699f7b44542e6a8345e26b202b
mIm1Ri <b2242070@photon.chitose.ac.jp>
2026-07-31 18:43:39 +0900

    分子図鑑のUI調整
    配置をそれっぽくしました。図鑑側に最低限の機能を実装しました。文字化け改善のため、フォントアセットを更新しました。

--- Assets/Scripts/MoleculeDetailPanel.cs
+++ Assets/Scripts/MoleculeDetailPanel.cs
@@ -2,9 +2,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Oculus.Platform;
+using System.Collections.Generic;
 
 public class MoleculeDetailPanel : MonoBehaviour
 {
+    private readonly Dictionary<ChemicalCategory, string> categoryNames =
+        new Dictionary<ChemicalCategory, string>()
+    {
+        { ChemicalCategory.Aliphatic,      "脂肪族" },
+        { ChemicalCategory.Aromatic,       "芳香族" },
+        { ChemicalCategory.Alcohol,        "アルコール" },
+        { ChemicalCategory.Phenol,         "フェノール類" },
+        { ChemicalCategory.Aldehyde,       "アルデヒド" },
+        { ChemicalCategory.Ketone,         "ケトン" },
+        { ChemicalCategory.CarboxylicAcid, "カルボン酸" },
+        { ChemicalCategory.Ester,          "エステル" },
+        { ChemicalCategory.NitroCompound,  "ニトロ化合物" },
+        { ChemicalCategory.Amine,          "アミン・アミド" }
+    };
+
+
     [Header("Text")]
     [SerializeField] private TMP_Text NameText;
     [SerializeField] private TMP_Text FormulaText;
@@ -22,7 +39,7 @@ public class MoleculeDetailPanel : MonoBehaviour
         FormulaText.text = Data.Formula;
         FunctionalGroupText.text = Data.FunctionalGroup;
         FunctionalGroupFormulaText.text  = Data.FunctionalGroupFormula;
-        CategoryText.text = Data.Categories.ToString();
+        CategoryText.text = CategoryToJapanese(Data.Categories);
         DescriptionText.text = Data.Description;
 
         StructuralFormulaImage.sprite = Data.StructuralFormula;
@@ -33,5 +50,19 @@ public class MoleculeDetailPanel : MonoBehaviour
     public void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    private string CategoryToJapanese(ChemicalCategory categories)
+    {
+        List<string> result = new List<string>();
+        foreach (var pair in categoryNames)
+        {
+            if (categories.HasFlag(pair.Key))
+            {
+                result.Add(pair.Value);
+            }
+        }
+
+        return string.Join("\n",result);
     }
 }
